5fe4729f2d2bba40388cc16721d609965347fbed
Saidur Rahman <devsaidur@gmail.com>
2023-10-18 03:53:24 +0600

    Loops,function with/without return

--- app/src/main/java/com/saidur/mykotline/loops.kt
+++ app/src/main/java/com/saidur/mykotline/loops.kt
@@ -25,4 +25,27 @@ fun main() {
     for (k in intdata) {
         println(k)
     }
+    //for loop with range
+    for(k in 5..7){
+        println(k)
+    }
+
+    //Functions
+    addition(2,6)
+    addition_with_return(12,10)
+    println("value return ${addition_shortReturn(40,14)}")
+    println("value returnShort ${addition_shortest_return(121,35)}")
 }
+
+//void functions
+fun addition(num :Int,num2:Int){
+    println(num+num2)
+}
+fun addition_with_return(num :Int,num2:Int){
+    return println("Total ${num.plus(num2)}")
+}
+fun addition_shortReturn(num:Int,num2: Int):Int
+{
+    return num+num2
+}
+fun addition_shortest_return(num: Int,num2: Int)=num+num2
