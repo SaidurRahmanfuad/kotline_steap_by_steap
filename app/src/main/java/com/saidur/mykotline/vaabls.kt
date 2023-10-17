84214e36f1d1e1f268a58a02582a3ad7c2206d97
Saidur Rahman <devsaidur@gmail.com>
2023-10-17 19:38:32 +0600

    Signed-off-by: Saidur Rahman <devsaidur@gmail.com>

---
+++ app/src/main/java/com/saidur/mykotline/vaabls.kt
@@ -0,0 +1,22 @@
+package com.saidur.mykotline
+
+fun main()
+{
+    //variable create var=changable,val=not changable,constant
+    var changable="rimon"
+    val name="Fuad"
+    val age="28"
+    val score=25
+    changable="masud"
+    println("My name is "+changable+" and i'm "+score+" years old")
+
+    //vaiable types
+    var designation : String
+    var x: Int
+    var y:Int
+    x=2
+    y=5
+    var sum = x + y
+    designation="Software"
+    println(designation+":"+sum)
+}
